8bd2edf89ed17999cfbb587a87ac8224586067d7
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-07 10:35:53 +0100

    fonction animate + réarangement

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.add("fade-in");
+});
+
 const b1 = document.getElementById('mailtouchable');/*
 const b2 = document.getElementById('mailtouchable');
 const b3 = document.getElementById('mailtouchable');
@@ -5,23 +9,24 @@ const b4 = document.getElementById('mailtouchable');*/
 
 const af1 = document.getElementById('mailAffichage');
 
+function animate(to_animate) {
+    if (to_animate.classList.contains('animate_on')) {
+        void to_animate.offsetWidth;
+        to_animate.classList.add('animate_off')
+        to_animate.classList.remove('animate_on')
+    }
+    else {
+        void to_animate.offsetWidth;
+        to_animate.classList.add('animate_on');
+        to_animate.classList.remove('animate_off');
+    }
+    return 0;
+}
+
 b1.addEventListener('click', () => {
     b1.classList.remove('animate-pop'); // si déjà animée
     void mail.offsetWidth; // "reset" l’animation
     b1.classList.add('animate-pop');
-
-    if (af1.classList.contains('animate_on')) {
-        void af1.offsetWidth;
-        af1.classList.add('animate_off')
-        af1.classList.remove('animate_on')
-    }
-    else {
-        void af1.offsetWidth;
-        af1.classList.add('animate_on');
-        af1.classList.remove('animate_off');
-    }
+    animate(af1);
 })
 
-window.addEventListener("DOMContentLoaded", () => {
-    document.body.classList.add("fade-in");
-});
